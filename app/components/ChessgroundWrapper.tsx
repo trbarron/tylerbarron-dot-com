3126d53d516105ecc362b2b99bf7494ec4b0f642
Tyler Barron <>
2024-10-13 11:07:27 -0600

    new zealand

--- app/components/ChessgroundWrapper.tsx
+++ app/components/ChessgroundWrapper.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Chessground as NativeChessground } from 'chessground';
+import { Chessground as NativeChessground } from 'chessground/src';
 
 // Make sure to import the CSS somewhere in your app
 // import 'chessground/assets/chessground.base.css';
