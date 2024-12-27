6c45d22774d481a7a57ec7cb93f7bab1ca81132e
Tyler Barron <trbbarron@gmail.com>
2024-12-27 08:51:43 -0700

    Add page title, favicon, google analytics

--- app/root.tsx
+++ app/root.tsx
@@ -10,9 +10,18 @@ import {
 
 import styles from './styles/index.css';
 
+const gaTrackingId = process.env.GA_TRACKING_ID;
+
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles }
 ];
+
+export const meta = () => {
+  return [
+    { title: "Barron Wasteland" },
+    { rel: "icon", href: "/favicon.ico" },
+  ];
+};
 
 export default function App() {
   return (
@@ -22,6 +31,19 @@ export default function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`} />
+        <script
+          async
+          id="gtag-init"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gaTrackingId}');
+            `,
+          }}
+        />
       </head>
       <body>
         <Outlet />
