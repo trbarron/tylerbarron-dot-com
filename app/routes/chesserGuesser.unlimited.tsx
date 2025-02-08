1a0b3bcf8858fef9d55708bfb94ef4762fbfb1a2
Tyler B <tdawgg2@yahoo.com>
2025-02-08 16:29:19 -0700

    text changes

--- app/routes/chesserGuesser.unlimited.tsx
+++ app/routes/chesserGuesser.unlimited.tsx
@@ -215,10 +215,10 @@ export default function ChesserGuesserUnlimited() {
               subtitle=""
             >
               <p>
-                Challenge yourself to guess the computer's evaluation of chess positions. Your goal is to estimate the position's value as accurately as possible -- extending your streak if you correctly guess the player ahead.
+                Challenge yourself to guess the computer's evaluation of chess positions. Your goal is to estimate the position's value as accurately as possible -- extending your streak if you correctly guess the player ahead
               </p>
               <p>
-                Each correct guess extends your streak. See how long you can maintain it by matching or closely approximating the computer's precision. It's a test of your chess judgment against the engine's calculations. Keep your streak going and sharpen your evaluative skills!
+                Each correct guess extends your streak. See how long you can maintain it by matching or closely approximating the computer's precision
               </p>
             </Subarticle>
           </Article>
