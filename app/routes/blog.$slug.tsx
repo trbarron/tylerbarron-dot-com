4d6200741ab7cd8ea7eec03ea1b96c8c166c254d
Tyler Barron <trbbarron@gmail.com>
2025-04-05 19:31:41 -0600

    added latex and updated the blog post

--- app/routes/blog.$slug.tsx
+++ app/routes/blog.$slug.tsx
@@ -54,6 +54,12 @@ export default function BlogPost() {
       <Navbar />
       <div className="max-w-4xl mx-auto px-4 py-8 bg-white">
         <article className="max-w-4xl mx-auto">
+          <link 
+            rel="stylesheet" 
+            href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" 
+            integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" 
+            crossOrigin="anonymous"
+          />
           {/* Header section - outside of prose context */}
           <header className="mb-8">
             <h1 className="text-3xl mb-4 text-gray-dark font-light">{frontmatter.title}</h1>
--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -2,11 +2,15 @@ import { bundleMDX } from 'mdx-bundler';
 import rehypeHighlight from 'rehype-highlight';
 import rehypeImgSize from 'rehype-img-size';
 import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export async function processMdx(source: string) {
   const result = await bundleMDX({
     source,
     mdxOptions(options) {
+      options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkMath];
+      options.rehypePlugins = [...(options.rehypePlugins ?? []), rehypeKatex];
       options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkGfm];
       options.rehypePlugins = [
         ...(options.rehypePlugins ?? []),
